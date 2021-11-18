8001d7d812e49f6d594bd26b996a6f0ef21a1fba
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-11-18 11:51:26 -0600

    change form, add function to create new bird, links with backend, redirect to success/index broken.

--- src/views/New.vue
+++ src/views/New.vue
@@ -1,20 +1,38 @@
 <template>
-  <div class="new">
- <p> Breed:   <input type="text"> </p>
- <p> Location:   <input type="text"> </p>
- <p> Image:   <input type="text"> </p>
- <p> Description:   <input type="text"> </p>
-<button type="button" class="btn btn-primary" v-on:click="newBirds()">Add Bird</button>
-  </div>
+ <div class="input-group input-group-lg">
+  <p><span class="input-group-text" id="basic-addon1">Breed</span>
+  <input type="text" class="form-control" placeholder="Breed" aria-label="Username" aria-describedby="basic-addon1" v-model="newBird.breed"></p>
+  <p><span class="input-group-text" id="basic-addon1">Location</span>
+  <input type="text" class="form-control" placeholder="Location" aria-label="Username" aria-describedby="basic-addon1" v-model="newBird.location"></p>
+  <p><span class="input-group-text" id="basic-addon1">Image URL</span>
+  <input type="text" class="form-control" placeholder="Link" aria-label="Username" aria-describedby="basic-addon1" v-model="newBird.image_url"></p>
+  <p><span class="input-group-text" id="basic-addon1">Description</span>
+  <input type="text" class="form-control" placeholder="Notes" aria-label="Username" aria-describedby="basic-addon1" v-model="newBird.description"></p>
+  <button type="button" class="btn btn-primary" v-on:click="createBirds()">Add Bird</button>
+
+</div>
 </template>
 
 <style></style>
 
 <script>
+import axios from 'axios';
   export default {
+    data: function () {
+      return {
+
+        newBird: {}
+      }
+
+    },
     methods: {
-newBirds: function () {
-  console.log('hello')
+createBirds: function () {
+  console.log('add a bird')
+
+axios.post("http://localhost:3000/birds", this.newBird).then(response => {
+console.log(response.data)
+
+})
 }
     },
   };
