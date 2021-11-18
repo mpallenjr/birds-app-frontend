9f8d5edc4706682c1d32b02987529260fd37ff1c
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-11-18 11:12:04 -0600

    link new page, add input lines, add create button, button link to function working.

---
+++ src/views/New.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="new">
+ <p> Breed:   <input type="text"> </p>
+ <p> Location:   <input type="text"> </p>
+ <p> Image:   <input type="text"> </p>
+ <p> Description:   <input type="text"> </p>
+<button type="button" class="btn btn-primary" v-on:click="newBirds()">Add Bird</button>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+  export default {
+    methods: {
+newBirds: function () {
+  console.log('hello')
+}
+    },
+  };
+</script>
