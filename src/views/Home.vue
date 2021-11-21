1fa7330a2196ef840a54715ece1bb0f1242fb972
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-11-21 11:39:27 -0600

    add vue2filters, add truncate to description, add filter to index on birds and replace bird in birds.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="home">
-    <div v-for="bird in birds">
+    <div  v-for="bird in filterBy(birds, 'golden')">
       <div class="card-text-center" style="width: 60rem;">
         <img v-bind:src="bird.image_url" class="card-img-top" v-bind:alt="bird.description">
         <div class="card-body">
           <h5 class="card-title">{{ bird.breed }}</h5>
-          <p class="card-text"> {{ bird.description }} </p>
+          <p class="card-text"> {{ bird.description | truncate(100) }} </p>
           <a href="#" class="btn btn-primary">More Info</a>
         </div>
       </div>
@@ -21,7 +21,9 @@
 
 <script>
 import axios from 'axios';
+import Vue2Filters from "vue2-filters";
   export default {
+    mixins: [Vue2Filters.mixin],
     data: function () {
       return {
         birds: []
